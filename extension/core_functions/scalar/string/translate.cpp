af409aaac845773a3c055049f2baeae23c3b7ef1
Florian Valeye <florian.valeye@gmail.com>
2026-08-30 19:43:41 +0200

    Build the translate mapping once when the needle and thread are constant

--- extension/core_functions/scalar/string/translate.cpp
+++ extension/core_functions/scalar/string/translate.cpp
@@ -13,27 +13,16 @@
 
 namespace duckdb {
 
-static string_t TranslateScalarFunction(const string_t &haystack, const string_t &needle, const string_t &thread,
-                                        vector<char> &result) {
-	// Get information about the haystack, the needle and the "thread"
-	auto input_haystack = haystack.GetData();
-	auto size_haystack = haystack.GetSize();
-
+static void BuildTranslateMap(const string_t &needle, const string_t &thread,
+                              unordered_map<int32_t, int32_t> &to_replace, unordered_set<int32_t> &to_delete) {
 	auto input_needle = needle.GetData();
 	auto size_needle = needle.GetSize();
-
 	auto input_thread = thread.GetData();
 	auto size_thread = thread.GetSize();
-
-	// Reuse the buffer
-	result.clear();
-	result.reserve(size_haystack);
-
 	idx_t i = 0, j = 0;
-	int sz = 0, c_sz = 0;
+	int sz = 0;
 
 	// Character to be replaced
-	unordered_map<int32_t, int32_t> to_replace;
 	while (i < size_needle && j < size_thread) {
 		auto codepoint_needle = Utf8Proc::UTF8ToCodepoint(input_needle, sz, size_needle - i);
 		input_needle += sz;
@@ -48,7 +37,6 @@ static string_t TranslateScalarFunction(const string_t &haystack, const string_t
 	}
 
 	// Character to be deleted
-	unordered_set<int32_t> to_delete;
 	while (i < size_needle) {
 		auto codepoint_needle = Utf8Proc::UTF8ToCodepoint(input_needle, sz, size_needle - i);
 		input_needle += sz;
@@ -58,12 +46,25 @@ static string_t TranslateScalarFunction(const string_t &haystack, const string_t
 			to_delete.insert(codepoint_needle);
 		}
 	}
+}
 
+static string_t TranslateScalarFunction(const string_t &haystack, const unordered_map<int32_t, int32_t> &to_replace,
+                                        const unordered_set<int32_t> &to_delete, vector<char> &result) {
+	auto input_haystack = haystack.GetData();
+	auto size_haystack = haystack.GetSize();
+
+	// Reuse the buffer
+	result.clear();
+	result.reserve(size_haystack);
+
+	idx_t i = 0;
+	int sz = 0, c_sz = 0;
 	char c[5] = {'\0', '\0', '\0', '\0', '\0'};
 	for (i = 0; i < size_haystack; i += UnsafeNumericCast<idx_t>(sz)) {
 		auto codepoint_haystack = Utf8Proc::UTF8ToCodepoint(input_haystack, sz, size_haystack - i);
-		if (to_replace.count(codepoint_haystack) != 0) {
-			Utf8Proc::CodepointToUtf8(to_replace[codepoint_haystack], c_sz, c);
+		auto replace_entry = to_replace.find(codepoint_haystack);
+		if (replace_entry != to_replace.end()) {
+			Utf8Proc::CodepointToUtf8(replace_entry->second, c_sz, c);
 			result.insert(result.end(), c, c + c_sz);
 		} else if (to_delete.count(codepoint_haystack) == 0) {
 			result.insert(result.end(), input_haystack, input_haystack + sz);
@@ -74,6 +75,14 @@ static string_t TranslateScalarFunction(const string_t &haystack, const string_t
 	return string_t(result.data(), UnsafeNumericCast<uint32_t>(result.size()));
 }
 
+static string_t TranslateScalarFunction(const string_t &haystack, const string_t &needle, const string_t &thread,
+                                        vector<char> &result) {
+	unordered_map<int32_t, int32_t> to_replace;
+	unordered_set<int32_t> to_delete;
+	BuildTranslateMap(needle, thread, to_replace, to_delete);
+	return TranslateScalarFunction(haystack, to_replace, to_delete, result);
+}
+
 static void TranslateFunction(DataChunk &args, ExpressionState &state, Vector &result) {
 	const auto &haystack_vector = args.data[0];
 	const auto &needle_vector = args.data[1];
@@ -81,6 +90,18 @@ static void TranslateFunction(DataChunk &args, ExpressionState &state, Vector &r
 
 	vector<char> buffer;
 	auto &heap = StringVector::GetStringHeap(result);
+	// a constant needle and thread describe the same mapping for every row, so build it once per chunk
+	if (needle_vector.GetVectorType() == VectorType::CONSTANT_VECTOR && !ConstantVector::IsNull(needle_vector) &&
+	    thread_vector.GetVectorType() == VectorType::CONSTANT_VECTOR && !ConstantVector::IsNull(thread_vector)) {
+		unordered_map<int32_t, int32_t> to_replace;
+		unordered_set<int32_t> to_delete;
+		BuildTranslateMap(*ConstantVector::GetData<string_t>(needle_vector),
+		                  *ConstantVector::GetData<string_t>(thread_vector), to_replace, to_delete);
+		UnaryExecutor::Execute<string_t, string_t>(haystack_vector, result, args.size(), [&](string_t input_string) {
+			return heap.AddString(TranslateScalarFunction(input_string, to_replace, to_delete, buffer));
+		});
+		return;
+	}
 	TernaryExecutor::Execute<string_t, string_t, string_t, string_t>(
 	    haystack_vector, needle_vector, thread_vector, result,
 	    [&](string_t input_string, string_t needle_string, string_t thread_string) {
